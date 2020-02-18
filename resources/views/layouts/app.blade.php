add1281488ebd794f61d5369b34232bc605c53c6
Wacu Mbugua <wmbugua@CIM.CO.KE>
2020-02-18 05:31:18 +0300

    validation errors and old data

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,6 +34,7 @@
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
                     <li><a href="/threads">All Threads</a></li>
+                        <li><a href="/threads/create">New Thread</a></li>
                         &nbsp;<li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Channels
                                 <span class="caret"></span></a>
